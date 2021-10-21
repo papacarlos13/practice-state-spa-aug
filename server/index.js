5c4c9bee00842599199fe25183b409daef1785c8
Carlos Mejia <papacarlos13@gmail.com>
2021-10-20 20:08:59 -0500

    Order Form to backend

--- server/index.js
+++ server/index.js
@@ -1,13 +1,45 @@
 const express = require("express");
+const pizzas = require("./routes/pizzas");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
 const app = express();
-//middleware 4-10
+
+mongoose.connect(process.env.MONGODB);
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
+
 const logging = (request, response, next) => {
   console.log(`${request.method} ${request.url} ${Date.now()}`);
   next();
 };
 
+// CORS Middleware
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
+
 app.use(express.json());
 app.use(logging);
+app.use(cors);
+app.use("/pizzas", pizzas);
 
 app
   .route("/status")
@@ -16,16 +48,6 @@ app
   })
   .post((request, response) => {
     response.json({ requestBody: request.body });
-  });
-
-app
-  .route("/pizzas")
-  .get((request, response) => {
-    // handle GET request
-    response.send(JSON.stringify({ message: "All pizzas" }));
-  })
-  .post((request, response) => {
-    response.send(JSON.stringify({ message: "Created pizza" }));
   });
 
 app.route("/users/:id").get((request, response) => {
